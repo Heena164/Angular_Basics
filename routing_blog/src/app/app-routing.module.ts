1ae8dfbb35d1cfc9c1818102dc765bb99e779d7b
Heena164 <heenasr164@gmail.com>
2023-12-11 23:20:20 +0530

    Routing

---
+++ routing_blog/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { NoPageComponent } from './no-page/no-page.component';
+
+const routes: Routes = [
+  {
+    component:AboutComponent,
+    path:'about'
+  },
+  {
+    component:UserComponent,
+    path:'user/:id'
+  },
+  {
+    component:HomeComponent,
+    path:''
+  },
+  {
+    component: NoPageComponent,
+    path:'**'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
